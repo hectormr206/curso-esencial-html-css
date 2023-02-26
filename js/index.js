9e5905430b8273d302283351ff2ab78cccf6eb91
Héctor Martínez <hectormartinezresendiz@gmail.com>
2023-02-26 03:28:29 -0600

    [FIX] Nemium B

--- js/index.js
+++ js/index.js
@@ -1,8 +1,34 @@
 const $detailsList = document.querySelectorAll("details");
 $detailsList.forEach(($details) => {
-  $details.addEventListener("click", (e) => {
+  $details.querySelector("summary").addEventListener("click", expand);
+  function expand() {
     $detailsList.forEach(($details) => {
       $details.removeAttribute("open");
     });
-  });
+  }
+});
+
+const $stylesA = document.querySelector("#styles-a");
+$stylesA.addEventListener("click", () => {
+  document.querySelector("#styles").href = "css/styles-a.css";
+});
+
+const $stylesB = document.querySelector("#styles-b");
+$stylesB.addEventListener("click", () => {
+  document.querySelector("#styles").href = "css/styles-b.css";
+});
+
+const $stylesC = document.querySelector("#styles-c");
+$stylesC.addEventListener("click", () => {
+  document.querySelector("#styles").href = "css/styles-c.css";
+});
+
+const $stylesD = document.querySelector("#styles-d");
+$stylesD.addEventListener("click", () => {
+  document.querySelector("#styles").href = "css/styles-d.css";
+});
+
+const $stylesE = document.querySelector("#styles-e");
+$stylesE.addEventListener("click", () => {
+  document.querySelector("#styles").href = "css/styles-e.css";
 });
